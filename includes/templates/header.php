7070badd498c8f7c54d21a6d3026d2abbd785eec
Acxel Morales <acxelmorales97@gmail.com>
2019-02-09 14:01:14 -0600

    Se agrego icono para el sitio

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Freelancer</title>
+    <link rel="icon" href="img/a.png">
     <!-- Normalize -->
     <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.0/normalize.css">
     <link href="https://fonts.googleapis.com/css?family=Krub:400,700" rel="stylesheet">
