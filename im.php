c3d2276a7e9cb18625ff3e9f328e2adb1a2a2f51
Acxel Morales <acxelmorales97@gmail.com>
2019-02-11 18:38:40 -0600

    Cambio de interfaz

--- im.php
+++ im.php
@@ -1,12 +1,31 @@
 <?php include_once './includes/templates/header.php'; ?>
 
-<div class="superior"> <!-- Adorno --> </div>
-<div class="foto"></div>
-<div class="inferior">
-    <div class="info-personal">
-        <p>Hola, mi nombre es Acxel Morales.<br>
-        Soy Técnico en Programación, y un apasionado de la tecnologia.<br>
-        Me fascina aprender lenguajes de programación y frameworks o librerias de JavaScript y Java<p>
+<div class="container-im">
+    <div class="info-im">
+        <div class="icons">
+            <i class="far fa-user"></i>
+            <p>Acxel Morales</p>
+        </div>
+        <div class="icons">
+            <i class="fas fa-school"></i>
+            <p>Técnico en Programación</p>
+        </div>
+        <div class="icons">
+            <i class="fas fa-mobile-alt"></i>
+            <p>55-34-07-25-19</p>
+        </div>
+        <div class="icons">
+            <i class="far fa-envelope"></i>
+            <p>acxelmorales97@gmail.com</p>
+        </div>
+    </div>
+    <div class="info-foto">
+        <div class="foto"></div>
+        <div class="info-desc">
+            <p>Hola, mi nombre es Acxel Morales.<br>
+            Soy Técnico en Programación, y un apasionado de la tecnologia.<br>
+            Me fascina aprender lenguajes de programación y frameworks o librerias de JavaScript y Java</p>
+        </div>
     </div>
 </div>
 
